0ba48d1d4c3c7b5f448bd8a93ecd8f05902669c9
GetacherD <getacher.d@gmail.com>
2023-02-07 10:49:37 +0300

    decode base64 data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ basic auth
 """
 from .auth import Auth
 from typing import Optional
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> Optional[str]:
+        """ decode_base64_authorization_header """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode("utf-8")).decode("utf-8")
+        except Exception:
+            return None
