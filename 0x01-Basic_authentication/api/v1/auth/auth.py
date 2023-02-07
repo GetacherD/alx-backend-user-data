ab011dd21adbc7c6f658747fcf41e4b90a4dc68c
GetacherD <getacher.d@gmail.com>
2023-02-07 09:39:21 +0300

    update require_login method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ decorator to make sure logged in """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        _path = path
+        if _path[-1] != "/":
+            _path = _path + "/"
+        if _path in excluded_paths:
+            print(_path, "in",  excluded_paths)
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header """
