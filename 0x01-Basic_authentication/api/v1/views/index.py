b88498e17a8e721826cb53ffe54cafc5f6752a93
GetacherD <getacher.d@gmail.com>
2023-02-07 08:58:49 +0300

    handle 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/api/v1/unauthorized", methods=["GET"], strict_slashes=False)
+def unauthorized_access() -> str:
+    """ unauthorized_access """
+    abort(401)
