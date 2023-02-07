3b74f79f6ada08ac374c967d9f71ec2af999599c
GetacherD <getacher.d@gmail.com>
2023-02-07 10:30:23 +0300

    extract base 64 data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,19 @@
 basic auth
 """
 from .auth import Auth
+from typing import Optional
 
 
 class BasicAuth(Auth):
     """ Basic Auth class """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> Optional[str]:
+        """ extract_base64_authorization_header """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
