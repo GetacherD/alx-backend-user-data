07951b59187c714c05cded2d7d793da48cb7ef50
GetacherD <getacher.d@gmail.com>
2023-02-08 15:20:59 +0300

    update exclude path regex match

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,7 @@ class Auth:
             return True
         if path[-1] != "/":
             path += "/"
-        if path.startswith(excluded_paths[0][:excluded_paths[0].index("*")]):
+        if path in excluded_paths or path.startswith(excluded_paths[0][:excluded_paths[0].index("*")]):
             return False
         return True
 
