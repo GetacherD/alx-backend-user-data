95ae7182c66a9c5eb2be2a9d1ecc872c3014732c
GetacherD <getacher.d@gmail.com>
2023-02-08 19:17:59 +0300

    update auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ session_authentication demo
 """
 from uuid import uuid4
 from .auth import Auth
+from api.v1.views.users import User
 
 
 class SessionAuth(Auth):
@@ -25,3 +26,13 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ get current_user """
+        cookie_val = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie_val)
+        user = User.get(user_id)
+        if user:
+            self.create_session(user_id)
+            return user
+        return None
