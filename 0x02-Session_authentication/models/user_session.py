c257a6a7577c4a23e91a8e072ce26fd696db058f
GetacherD <getacher.d@gmail.com>
2023-02-09 14:43:02 +0300

    test

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,5 +10,6 @@ class UserSession(Base):
 
     def __init__(self, *args: list, **kwargs: dict):
         """ initialize"""
-        self.user_id = ""
-        self.session_id = ""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
