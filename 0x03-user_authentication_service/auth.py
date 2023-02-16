11d69d3d34b726e7eae0d93c5184c2e750e8b3fe
GetacherD <getacher.d@gmail.com>
2023-02-16 04:20:51 +0300

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,9 +42,10 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if bcrypt.checkpw(password.encode("utf-8"), user.hashed_password):
                 return True
+            else:
+                return False
         except Exception:
             return False
-        return False
 
     def create_session(self, email: str) -> Optional[str]:
         """ create session id for user """
