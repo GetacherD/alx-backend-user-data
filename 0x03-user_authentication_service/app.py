2cdebad981547023ea1b48964d709c76a99cd9d5
GetacherD <getacher.d@gmail.com>
2023-02-16 08:21:17 +0300

    test

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,8 +88,6 @@ def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    if not reset_token or not new_password or not email:
-        abort(400)
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
